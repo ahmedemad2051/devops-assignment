6f18c24230e78f71480ec1705271d744da8ad05d
ahmed emad <ahmed.emad2051@gmail.com>
2021-03-11 18:36:11 +0200

    remove unused variables

--- EKS-with-terraform/variables.tf
+++ EKS-with-terraform/variables.tf
@@ -8,62 +8,18 @@ variable "vpc_cidr" {
   type    = string
 }
 
-variable "vpc_tag" {
-  type    = string
+
+########## public subnets #############
+variable "pub_subnets" {
+  type    = list(string)
 }
 
-########## public subnet 1 #############
-variable "pub_sub_1_cidr" {
-  type    = string
+########## private subnets #############
+
+variable "pv_subnets" {
+  type    = list(string)
 }
 
-variable "pub_sub_1_zone" {
-  type    = string
-}
-
-variable "pub_sub_1_tag" {
-  type    = string
-}
-
-
-########## public subnet 2 #############
-variable "pub_sub_2_cidr" {
-  type    = string
-}
-
-variable "pub_sub_2_zone" {
-  type    = string
-}
-
-variable "pub_sub_2_tag" {
-  type    = string
-}
-
-########## private subnet 1 #############
-variable "pv_sub_1_cidr" {
-  type    = string
-}
-
-variable "pv_sub_1_zone" {
-  type    = string
-}
-
-variable "pv_sub_1_tag" {
-  type    = string
-}
-
-########## private subnet 2 #############
-variable "pv_sub_2_cidr" {
-  type    = string
-}
-
-variable "pv_sub_2_zone" {
-  type    = string
-}
-
-variable "pv_sub_2_tag" {
-  type    = string
-}
 
 ############### eks ##################
 variable "cluster_name" {
--- EKS-with-terraform/vpc.tf
+++ EKS-with-terraform/vpc.tf
@@ -1,13 +1,3 @@
-# Create a VPC
-#resource "aws_vpc" "eks-vpc" {
-#  cidr_block = var.vpc_cidr
-#    tags = {
-#    Name = var.vpc_tag
-#  }
-#}
-
-
-
 data "aws_availability_zones" "available" {}
 
 
@@ -16,25 +6,11 @@ module "vpc" {
   version = "2.6.0"
 
   name                 = "eks-vpc"
-  cidr                 = "10.0.0.0/16"
+  cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.3.0/24", "10.0.4.0/24"]
+  private_subnets      = var.pv_subnets
+  public_subnets       = var.pub_subnets
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
-
-  tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-  }
 }
